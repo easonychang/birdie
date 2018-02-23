05a31003a2bcf622b36372f482a3493400363dfd
v1nnyc <cannallav019@yahoo.com>
2018-02-22 19:12:15 -0800

    added events to calendar

--- routes/confirm.js
+++ routes/confirm.js
@@ -1,17 +1,9 @@
-var data = require("../events.json");
+
 /*
  * GET confirm page.
  */
 
 exports.confirmView = function(req, res){
-	newEvent =	{
 
-			"start" : req.query.time,
-			"place" : req.query.place,
-			"friend" : req.query.friend
-
-		};
-
-	data.events.push(newEvent);
-	res.render('confirm');
-}
+  res.render('confirm');
+};
