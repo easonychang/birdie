c896ba0c18fe775856f8b299dfbc03a4a9218e62
v1nnyc <cannallav019@yahoo.com>
2018-03-07 16:03:33 -0800

    info is updated dynamically

--- routes/info.js
+++ routes/info.js
@@ -11,19 +11,109 @@ exports.infoView = function(req, res){
   	res.render('info');
 };
 
+function displayData(response){
+
+    /*var newRest = {
+        "id": response.jsonBody.id,
+        "name": response.jsonBody.name,
+        "image_url": response.jsonBody.image_url,
+        "is_claimed": response.jsonBody.is_claimed,
+        "is_closed": response.jsonBody.is_closed,
+        "url": response.jsonBody.url,
+        "phone": response.jsonBody.phone,
+        "display_phone": response.jsonBody.display_phone,
+        "review_count": response.jsonBody.review_count,
+        "categories": response.jsonBody.categories,
+        "rating": response.jsonBody.rating,
+        "location": response.jsonBody.location,
+        "coordinates": response.jsonBody.coordinates,
+        "photos": response.jsonBody.photos,
+        "price": response.jsonBody.price,
+        "hours": response.jsonBody.hours,
+        "transactions": response.jsonBody.transactions
+    };*/
+
+    var rating_image;
+    switch (response.jsonBody.rating) {
+    case 0:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_0.png';
+        break;
+    case 1:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_1.png';
+        break;
+    case 1.5:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_1_half.png';
+        break;
+    case 2:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_2.png';
+        break;
+    case 2.5:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_2_half.png';
+        break;
+    case 3:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_3.png';
+        break;
+    case 3.5:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_3_half.png';
+        break;
+    case 4:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_4.png';
+        break;
+    case 4.5:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_4_half.png';
+        break;
+    case 5:
+        rating_image = '/images/yelp_stars/web_and_ios/regular/regular_5.png';
+        
+    } 
+
+    var locationArr = response.jsonBody.location.display_address;
+    var address="";
+    
+    for (var index in locationArr){
+        address += locationArr[index]+" ";
+    }
+
+    var display = {
+        "name": response.jsonBody.name,
+        "image_url": response.jsonBody.image_url,
+        "categories": response.jsonBody.categories[0].title,
+        "review_count": response.jsonBody.review_count,
+        "rating": response.jsonBody.rating,
+        "rating_img" : rating_image,
+        "price": response.jsonBody.price,
+        "location": address,
+
+    };
+
+
+     displaydata.restaurant.push(display);
+
+}
+
+
 exports.getRec = function (req,res){
+	displaydata.restaurant.length = 0;
     var searchValue = req.params.restaurant;
 
     const searchRequest = {
       term: searchValue,
       location: 'san diego, ca'
     };
+
     const client = yelp.client(apiKey);
 
     client.search(searchRequest).then(response => {
         const result = response.jsonBody.businesses;
-        console.log(result[0].location.address1);
-        res.render('info', result[0]);
+        //res.render('info', result);
+        client.business(result[0].id).then(response => {
+                            displayData(response);
+                            res.render('info', displaydata);
+                            //res.json(displaydata);
+
+                        }).catch(e => {
+                            console.log(e);
+                        }); //result[0] end 
   });
 }
 
